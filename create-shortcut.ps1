fde2cb299237518594a155736b57b9d123731951
chius-me <chius.me@outlook.com>
2026-05-28 08:54:10 +0800

    del something

--- create-shortcut.ps1
+++
@@ -1,7 +0,0 @@
-$ws = New-Object -ComObject WScript.Shell
-$sc = $ws.CreateShortcut("$([Environment]::GetFolderPath('Desktop'))\Push Post.lnk")
-$sc.TargetPath = "pwsh.exe"
-$sc.Arguments = "-NoExit -ExecutionPolicy Bypass -File `"$PSScriptRoot\push.ps1`""
-$sc.WorkingDirectory = $PSScriptRoot
-$sc.IconLocation = "pwsh.exe,0"
-$sc.Save()
--- push.ps1
+++
@@ -1,3 +0,0 @@
-git add -A
-git commit -m "post update"
-git push origin HEAD
